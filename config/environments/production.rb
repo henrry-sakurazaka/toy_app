f91e23f27d243325b17bb1b7282c64a66f9c168f
henrry-sakurazaka <bright.lights.21@me.com>
2025-07-24 23:05:53 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # これがないと assets:precompile が失敗する
+  config.assets.compile = true
 end
